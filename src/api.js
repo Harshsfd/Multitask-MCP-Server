db9b815ec51f5ffc8d9f80a2cc32cd1492981ac7
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-04 19:12:18 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8000"; // backend URL
+// export const BASE_URL = "http://127.0.0.1:8000"; // backend URL
+export const BASE_URL = "https://mcp-server-qko7.onrender.com"
 export const api = axios.create({
   baseURL: BASE_URL,
 });
