55009ff7fb65171679c1f64c746a033e05a390cb
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-04 20:02:13 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,6 @@
-// src/api.js
 import axios from "axios";
 
-// Fallback: अगर env var set नहीं है तो localhost:8000 use होगा
-export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000";
-
+export const BASE_URL = "http://127.0.0.1:8000"; // backend URL
 export const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
-  headers: { "Content-Type": "application/json" },
 });
